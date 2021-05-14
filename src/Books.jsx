fbf171cb0fc2fc85aabe5e0c314df0242b43d60a
Marek <mareklubomirski@me.com>
2021-05-14 14:57:19 -0700

    fix(books): layout styles

--- src/Books.jsx
+++ src/Books.jsx
@@ -5,32 +5,26 @@ const Books = ({ books }) => {
   return (
     <div className="card-container">
       {books?.map((book) => (
-        <div key={book.id} className="book-card" >
+        <div key={book.id} className="book-card">
           <div className="image-container">
-          <a target="blank" href={book.volumeInfo.previewLink}>
-            <img
-              src={book.volumeInfo.imageLinks.thumbnail}
-              alt={book.volumeInfo.title}
-            />
+            <a target="blank" href={book.volumeInfo.previewLink}>
+              <img
+                src={book.volumeInfo.imageLinks.thumbnail}
+                alt={book.volumeInfo.title}
+              />
             </a>
           </div>
           <div className="card-content">
             <div className="card-title">
               <h3>{book.volumeInfo.title}</h3>
             </div>
-            <div className="card-body">
-              {/* <p>:)</p> */}
-            </div>
-            </div>
-            <div className="btn">
-              <button>
-                <a href={book.volumeInfo.previewLink}>
-                  Find out more
-               </a>
-              </button>
-            </div>
-          
-          
+            <div className="card-body">{/* <p>:)</p> */}</div>
+          </div>
+          <div className="btn">
+            <button>
+              <a href={book.volumeInfo.previewLink}>More Info</a>
+            </button>
+          </div>
         </div>
       ))}
     </div>
